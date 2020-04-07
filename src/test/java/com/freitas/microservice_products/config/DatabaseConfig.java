8af414ed6bf262e30f5a7ed8d71b3cb5c1549033
Claudenir Freitas <claudenir.machado@sensedia.com>
2020-04-07 17:51:53 +0100

    up na versao do Quarkus e TestContainers, alterando a abordagem para execucao dos testes de integracao

---
+++ src/test/java/com/freitas/microservice_products/config/DatabaseConfig.java
@@ -0,0 +1,39 @@
+package com.freitas.microservice_products.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.flywaydb.core.Flyway;
+import org.testcontainers.containers.MySQLContainer;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+
+public class DatabaseConfig implements QuarkusTestResourceLifecycleManager {
+
+	private static final MySQLContainer<?> CONTAINER = new MySQLContainer<>();
+
+	@Override
+	public Map<String, String> start() {
+		CONTAINER.start();
+		
+		Flyway.configure()
+	          .dataSource(CONTAINER.getJdbcUrl(), CONTAINER.getUsername(), CONTAINER.getPassword())
+	          .load()
+		      .migrate();
+		
+		Map<String, String> properties = new HashMap<>();
+		properties.put("quarkus.datasource.url", CONTAINER.getJdbcUrl());
+		properties.put("quarkus.datasource.username", CONTAINER.getUsername());
+		properties.put("quarkus.datasource.password", CONTAINER.getPassword());
+
+		return properties;
+	}
+
+	@Override
+	public void stop() {
+		if (CONTAINER.isRunning()) {
+			CONTAINER.stop();
+		}
+	}
+
+}
--- src/test/java/com/freitas/microservice_products/integration/ProductResourceIntegrationTest.java
+++ src/test/java/com/freitas/microservice_products/integration/ProductResourceIntegrationTest.java
@@ -7,43 +7,25 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 
-import org.flywaydb.core.Flyway;
 import org.jboss.resteasy.util.HttpHeaderNames;
 import org.junit.Assert;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.freitas.microservice_products.containers.MySqlContainerTest;
+import com.freitas.microservice_products.config.DatabaseConfig;
 import com.freitas.microservice_products.dto.ProductDTO;
 import com.freitas.microservice_products.models.Product;
 import com.freitas.microservice_products.services.ProductService;
 
+import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
 @QuarkusTest
+@QuarkusTestResource(value = DatabaseConfig.class)
 public class ProductResourceIntegrationTest {
 
-	private static final MySqlContainerTest CONTAINER = new MySqlContainerTest();
-	
 	@Inject
 	ProductService service;
-
-	@BeforeAll
-	public static void init() {
-		CONTAINER.start();
-
-		Flyway.configure()
-		      .dataSource(CONTAINER.getJdbcUrl(), CONTAINER.getUsername(), CONTAINER.getPassword())
-		      .load()
-			  .migrate();
-	}
-
-	@AfterAll
-	public static void teardown() {
-		CONTAINER.stop();
-	}
 
 	@Test
 	public void testGetSuccess() {
