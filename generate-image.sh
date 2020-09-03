59df8d5435539f92b2fe9404ae608091f13c1ee9
Claudenir Freitas <claudenir.machado@sensedia.com>
2020-09-03 07:57:28 +0100

    fix pipeline

--- generate-image.sh
+++ generate-image.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-mvn clean install -Pnative -DskipTests
+mvn clean package -DskipTests
+
+cp target/quarkus-microservice-test-1.0.0-SNAPSHOT-runner.jar docker/app.jar
 
 docker build -t tdc2019/quarkus-microservice-test:latest .
